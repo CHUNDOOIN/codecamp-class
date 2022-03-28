ae08f626d9b3ec5533df3587af2aa7e173fba6e4
CHUNDOOIN <chun.doo.in@gmail.com>
2022-03-28 17:19:36 +0900

    22.03.28 수업

---
+++ class/pages/11-02-library-star/index.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Rate } from "antd";
+
+export default function LibraryStarPage() {
+  const [value, setValue] = useState(3);
+
+  const handleChange = (value: any) => {
+    setValue(value);
+    alert(`별점은 ${value}점 입니다.`);
+    console.log(`별점은 ${value}점 입니다.`);
+  };
+
+  return (
+    <div>
+      <Rate onChange={handleChange} value={value} />
+      <div>{value}점</div>
+    </div>
+  );
+}
+
---
+++ class/pages/11-03-library-youtube/index.tsx
@@ -0,0 +1,5 @@
+import ReactPlayer from "react-player";
+
+export default function LibraryYoutubePage() {
+  return <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />;
+}
