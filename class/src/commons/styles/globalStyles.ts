574dc380aedd4b88f36bda1e3a9fb2f1c5a44bef
CHUNDOOIN <chun.doo.in@gmail.com>
2022-04-28 00:26:32 +0900

    22.04.28 수업

--- class/src/commons/styles/globalStyles.ts
+++ class/src/commons/styles/globalStyles.ts
@@ -5,11 +5,11 @@ export const globalStyles = css`
     margin: 0;
     box-sizing: border-box;
     font-size: 15px;
-    font-family: "myfont";
+    /* font-family: "myfont"; */
   }
 
-  @font-face {
+  /* @font-face {
     font-family: "myfont";
-    src: url("/fonts/scifibit.ttf");
+    src: url("/fonts/scifibit.ttf"); */
   }
 `;
